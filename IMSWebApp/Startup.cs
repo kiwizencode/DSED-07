afd88610d6006647bf8332e868762f9f33eeaefc
KEUM HONG LIM <kiwizen.code@hotmail.com>
2017-11-01 23:27:56 +1300

    scaffolded MORTALITY table

--- IMSWebApp/Startup.cs
+++ IMSWebApp/Startup.cs
@@ -46,7 +46,7 @@ namespace IMSWebApp
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=SPECIES}/{action=Index}/{id?}");  //"{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=MORTALITies}/{action=Index}/{id?}");  //"{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
