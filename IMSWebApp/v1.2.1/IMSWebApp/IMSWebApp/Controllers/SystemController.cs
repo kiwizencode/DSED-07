ac299070b562aa2dbfe0bcc5d3c0557e8debc717
KEUM HONG LIM <kiwizen.code@hotmail.com>
2017-11-08 23:34:20 +1300

    start writing JavaScript to display list of speices onto the pages

---
+++ IMSWebApp/v1.2.1/IMSWebApp/IMSWebApp/Controllers/SystemController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IMSWebApp.Controllers
+{
+    //[Produces("application/json")]
+    //[Route("api/System")]
+    public class SystemController : Controller
+    {
+        public IActionResult Species()
+        {
+            return View();
+        }
+    }
+}
