b45bd695b54ad8196f77806cd43b173031b86aec
KEUM HONG LIM <kiwizen.code@hotmail.com>
2017-11-12 10:24:24 +1300

    save change : Index page for Tank Logs

---
+++ IMSWebApp/Controllers/TankLogsController.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using IMSWebApp.Models;
+
+namespace IMSWebApp.Controllers
+{
+    public class TankLogsController : Controller
+    {
+        private readonly fishinaboxContext _context;
+
+        public TankLogsController(fishinaboxContext context)
+        {
+            _context = context;
+        }
+
+        // GET: TankLogs
+        public async Task<IActionResult> Index()
+        {
+            var fishinaboxContext = _context.TankLog.Include(t => t.PeriodFkNavigation).Include(t => t.SizeFkNavigation).Include(t => t.SpeciesFkNavigation).Include(t => t.StuffFkNavigation).Include(t => t.TankFkNavigation);
+            return View(await fishinaboxContext.ToListAsync());
+        }
+
+        // GET: TankLogs/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tankLog = await _context.TankLog
+                .Include(t => t.PeriodFkNavigation)
+                .Include(t => t.SizeFkNavigation)
+                .Include(t => t.SpeciesFkNavigation)
+                .Include(t => t.StuffFkNavigation)
+                .Include(t => t.TankFkNavigation)
+                .SingleOrDefaultAsync(m => m.IdPk == id);
+            if (tankLog == null)
+            {
+                return NotFound();
+            }
+
+            return View(tankLog);
+        }
+
+        // GET: TankLogs/Create
+        public IActionResult Create()
+        {
+            ViewData["PeriodFk"] = new SelectList(_context.MovementPeriod, "IdPk", "IdPk");
+            ViewData["SizeFk"] = new SelectList(_context.RecordPetSize, "IdPk", "Description");
+            ViewData["SpeciesFk"] = new SelectList(_context.MarineSpecies, "IdPk", "Common");
+            ViewData["StuffFk"] = new SelectList(_context.SysStuff, "IdPk", "IdCode");
+            ViewData["TankFk"] = new SelectList(_context.Tank, "IdPk", "IdCode");
+            return View();
+        }
+
+        // POST: TankLogs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdPk,PeriodFk,TankFk,SpeciesFk,SpeciesText,SpeciesText2,Qty,Comment,StuffFk,OrderFk,SizeFk")] TankLog tankLog)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tankLog);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PeriodFk"] = new SelectList(_context.MovementPeriod, "IdPk", "IdPk", tankLog.PeriodFk);
+            ViewData["SizeFk"] = new SelectList(_context.RecordPetSize, "IdPk", "Description", tankLog.SizeFk);
+            ViewData["SpeciesFk"] = new SelectList(_context.MarineSpecies, "IdPk", "Common", tankLog.SpeciesFk);
+            ViewData["StuffFk"] = new SelectList(_context.SysStuff, "IdPk", "IdCode", tankLog.StuffFk);
+            ViewData["TankFk"] = new SelectList(_context.Tank, "IdPk", "IdCode", tankLog.TankFk);
+            return View(tankLog);
+        }
+
+        // GET: TankLogs/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tankLog = await _context.TankLog.SingleOrDefaultAsync(m => m.IdPk == id);
+            if (tankLog == null)
+            {
+                return NotFound();
+            }
+            ViewData["PeriodFk"] = new SelectList(_context.MovementPeriod, "IdPk", "IdPk", tankLog.PeriodFk);
+            ViewData["SizeFk"] = new SelectList(_context.RecordPetSize, "IdPk", "Description", tankLog.SizeFk);
+            ViewData["SpeciesFk"] = new SelectList(_context.MarineSpecies, "IdPk", "Common", tankLog.SpeciesFk);
+            ViewData["StuffFk"] = new SelectList(_context.SysStuff, "IdPk", "IdCode", tankLog.StuffFk);
+            ViewData["TankFk"] = new SelectList(_context.Tank, "IdPk", "IdCode", tankLog.TankFk);
+            return View(tankLog);
+        }
+
+        // POST: TankLogs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdPk,PeriodFk,TankFk,SpeciesFk,SpeciesText,SpeciesText2,Qty,Comment,StuffFk,OrderFk,SizeFk")] TankLog tankLog)
+        {
+            if (id != tankLog.IdPk)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tankLog);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TankLogExists(tankLog.IdPk))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PeriodFk"] = new SelectList(_context.MovementPeriod, "IdPk", "IdPk", tankLog.PeriodFk);
+            ViewData["SizeFk"] = new SelectList(_context.RecordPetSize, "IdPk", "Description", tankLog.SizeFk);
+            ViewData["SpeciesFk"] = new SelectList(_context.MarineSpecies, "IdPk", "Common", tankLog.SpeciesFk);
+            ViewData["StuffFk"] = new SelectList(_context.SysStuff, "IdPk", "IdCode", tankLog.StuffFk);
+            ViewData["TankFk"] = new SelectList(_context.Tank, "IdPk", "IdCode", tankLog.TankFk);
+            return View(tankLog);
+        }
+
+        // GET: TankLogs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tankLog = await _context.TankLog
+                .Include(t => t.PeriodFkNavigation)
+                .Include(t => t.SizeFkNavigation)
+                .Include(t => t.SpeciesFkNavigation)
+                .Include(t => t.StuffFkNavigation)
+                .Include(t => t.TankFkNavigation)
+                .SingleOrDefaultAsync(m => m.IdPk == id);
+            if (tankLog == null)
+            {
+                return NotFound();
+            }
+
+            return View(tankLog);
+        }
+
+        // POST: TankLogs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var tankLog = await _context.TankLog.SingleOrDefaultAsync(m => m.IdPk == id);
+            _context.TankLog.Remove(tankLog);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TankLogExists(int id)
+        {
+            return _context.TankLog.Any(e => e.IdPk == id);
+        }
+    }
+}
--- IMSWebApp/Models/TankLog.cs
+++ IMSWebApp/Models/TankLog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace IMSWebApp.Models
 {
@@ -11,21 +12,32 @@ namespace IMSWebApp.Models
         }
 
         public int IdPk { get; set; }
+        [Display(Name = "Date")]
         public int? PeriodFk { get; set; }
         public int TankFk { get; set; }
+        [Display(Name = "Species")]
         public int? SpeciesFk { get; set; }
+        [Display(Name = "Description 1")]
         public string SpeciesText { get; set; }
+        [Display(Name = "Description 2")]
         public string SpeciesText2 { get; set; }
         public int Qty { get; set; }
         public string Comment { get; set; }
+        [Display(Name = "Stuff")]
         public int StuffFk { get; set; }
         public int? OrderFk { get; set; }
+        [Display(Name = "Size")]
         public int? SizeFk { get; set; }
 
+        [Display(Name = "Period")]
         public MovementPeriod PeriodFkNavigation { get; set; }
+        [Display(Name = "Size")]
         public RecordPetSize SizeFkNavigation { get; set; }
+        [Display(Name = "Species")]
         public MarineSpecies SpeciesFkNavigation { get; set; }
+        [Display(Name = "Stuff")]
         public SysStuff StuffFkNavigation { get; set; }
+        [Display(Name = "Tank")]
         public Tank TankFkNavigation { get; set; }
         public ICollection<TankLogDaily> TankLogDaily { get; set; }
     }
